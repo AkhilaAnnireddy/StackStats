86a9e5b26a7d2ce22d5e8ea54b87533530238d05
anushanagula <nagulaanusha21@gmail.com>
2021-03-29 12:19:04 +0530

    navbar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import Searchbar from '../Pages/Landingpage/Landingpage'
+import { Link } from "react-router-dom";
+import { withRouter } from 'react-router-dom';
+import Landingpage from "../Pages/Landingpage/Landingpage";
+
+class Navbar extends Component{
+    render(){
+        return(
+            <header>
+			<div >
+				<nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+					<Link className='navbar-brand text-white logo' to='/'>
+						
+						
+                        DevStacks
+					</Link>
+
+            
+					<div className=" navbar-extras">
+                        <Landingpage/>
+                    </div>
+                    </nav>
+			</div>
+		</header>
+	);
+    }
+}
+export default Navbar;
