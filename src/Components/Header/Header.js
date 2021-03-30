a665e9cfb9453faec540825954f30c4893dbd18f
anushanagula <nagulaanusha21@gmail.com>
2021-03-30 13:56:06 +0530

    navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,26 +4,53 @@ import { Link } from "react-router-dom";
 import { withRouter } from 'react-router-dom';
 import Landingpage from "../Pages/Landingpage/Landingpage";
 
-class Navbar extends Component{
+class Header extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            username:this.props.username
+          }
+    }
+    handleSearch(e) {
+        this.setState({ username: e.target.value })
+      }
+      handleGoClick(e)
+      {
+        e.preventDefault()
+        this.props.history.push({pathname:'/profile',state:{username: this.state.username}});
+          
+      }
     render(){
         return(
             <header>
 			<div >
 				<nav className='navbar navbar-expand-md navbar-dark bg-dark'>
-					<Link className='navbar-brand text-white logo' to='/'>
-						
-						
+					<Link className='navbar-brand text-white logo' to='/'>	
                         DevStacks
 					</Link>
-
-            
-					<div className=" navbar-extras">
-                        <Landingpage/>
+                    <div className='navbar-extras'>
+                    <div className='searchbar-container'>
+                        <form className='example' onSubmit={e => e.preventDefault()}>
+                        <h1>Dev-Stacks</h1>
+                        <input
+                            type='text'
+                            size='45'
+                            placeholder='Find Stackoverflow users...'
+                            onChange={this.handleSearch.bind(this)}
+                            value={this.state.username} />
+                            <button
+                            type='submit'
+                            onClick={this.handleGoClick.bind(this)}>
+                            <i class="fas fa-search"></i>
+                        </button>
+                        </form>
                     </div>
+                    </div>
+
                     </nav>
 			</div>
 		</header>
 	);
     }
 }
-export default Navbar;
+export default withRouter(Header);
