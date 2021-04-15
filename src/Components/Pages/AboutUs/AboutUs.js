64068d3d529dab0ce9d05cb4cb91ed40cbe685b6
Dinesh Pandikona <53090203+Tesla-Atoz@users.noreply.github.com>
2021-04-15 16:35:50 +0530

    updated the linkedIn link

--- src/Components/Pages/AboutUs/AboutUs.js
+++ src/Components/Pages/AboutUs/AboutUs.js
@@ -31,7 +31,7 @@ class AboutUs extends Component{
             <div className='card'>
                 <AboutUsCard img={Akhila} name="Akhila Annireddy" github="https://github.com/AkhilaAnnireddy" linkedin="https://www.linkedin.com/in/akhila-annireddy-900632198"/>
                 <AboutUsCard img={Anusha} name="Anusha Nagula" github="https://github.com/anushanagula" linkedin="https://www.linkedin.com/in/anusha-nagula-7281b2192"/>
-                <AboutUsCard img={Dinesh} name="Dinesh Pandikona" github="https://github.com/Tesla-Atoz" linkedin=""/>
+                <AboutUsCard img={Dinesh} name="Dinesh Pandikona" github="https://github.com/Tesla-Atoz" linkedin="blabla"/>
                 <AboutUsCard img={Chandana} name="Ravi Chandana Golla" github="https://github.com/chandana-0904" linkedin="https://www.linkedin.com/in/ravi-chandana-golla-0904"/>
                 <AboutUsCard img={Supraja} name="Supraja Alleni" github="https://github.com/SuprajaAlleni" linkedin="https://www.linkedin.com/in/supraja-alleni-90358016a"/>
             
