7f61630fb066b212accfd39084c0da9a04a21b2f
supraja <suprajaalleni123@gmail.com>
2021-04-19 22:29:28 +0530

    about_commit2

--- src/Components/Pages/AboutUs/AboutUs.js
+++ src/Components/Pages/AboutUs/AboutUs.js
@@ -37,33 +37,22 @@ class AboutUs extends Component{
 
             <div className='card abtinline'>
             <div className="row">
-                <div className="col-md-4 justify-content-center">
+                <div className="col-md justify-content-center">
                 <AboutUsCard img={Akhila} name="Akhila Annireddy" github="https://github.com/AkhilaAnnireddy" linkedin="https://www.linkedin.com/in/akhila-annireddy-900632198" role="Web Developer"/>
                 </div>
-                <div className="col-md-4 justify-content-center">
+                <div className="col-md justify-content-center">
                 <AboutUsCard img={Anusha} name="Anusha Nagula" github="https://github.com/anushanagula" linkedin="https://www.linkedin.com/in/anusha-nagula-7281b2192" role="Web Developer"/>
                 </div>
-                <div className="col-md-4 justify-content-center">
-                <AboutUsCard img={Dinesh} name="Dinesh Pandikona" github="https://github.com/Tesla-Atoz" linkedin="blabla" role="Web Developer" role="Team Leader"/>
+                <div className="col-md justify-content-center">
+                <AboutUsCard img={Dinesh} name="Dinesh Pandikona" github="https://github.com/Tesla-Atoz" linkedin="" role="Web Developer" role="Team Leader"/>
                 </div>
-            </div>
 
-            <div className="row">
-            <div className="col-md-2 justify-content-center">
-
-            </div>
-            <div className="col-md-2 justify-content-center">
+            <div className="col-md justify-content-center">
             <AboutUsCard img={Chandana} name="Ravi Chandana Golla" github="https://github.com/chandana-0904" linkedin="https://www.linkedin.com/in/ravi-chandana-golla-0904" role="Web Developer"/>
             </div>
-            <div className="col-md-2 justify-content-center">
-
-</div>
-            <div className="col-md-2 justify-content-center">
+            <div className="col-md justify-content-center">
             <AboutUsCard img={Supraja} name="Supraja Alleni" github="https://github.com/SuprajaAlleni" linkedin="https://www.linkedin.com/in/supraja-alleni-90358016a" role="Web Developer"/>
             </div>
-            <div className="col-md-2 justify-content-center">
-
-</div>
 
             </div>
 
